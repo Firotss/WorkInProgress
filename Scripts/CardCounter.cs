1a20fd1493d330d6b2018f177ad130b17e973bd6
Firotss <77095260+Firotss@users.noreply.github.com>
2026-01-31 02:22:01 +0200

    Update CardCounter.cs

--- Scripts/CardCounter.cs
+++ Scripts/CardCounter.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class CardCounter : MonoBehaviour
 {
@@ -15,30 +16,39 @@ public class CardCounter : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Card newCard = DeckManager.Instance.GetFirstCard();
-            if (newCard != null)
-            {
-                int nextSlot = handcards.Count;
-                handcards.Add(nextSlot, newCard);
-                Debug.Log($"Player get: {newCard.name}. Count in hand: {handcards.Count}");
-            }
-            else
-            {
-                Debug.Log("Deck is empty");
-            }
+            Debug.Log("--- Filling cards... ---");
+            FillCards();
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PlayCardFromHandToBoard();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            BurnCardFromBoard();
         }
     }
 
     public void FillCards()
     {
         int maxHandCards = CountMaxHandCards();
+
         while (handcards.Count < maxHandCards)
         {
             Card newCard = DeckManager.Instance.GetFirstCard();
-            if (newCard == null) break;
+            
+            if (newCard == null) 
+            {
+                break;
+            }
 
-            int newIndex = handcards.Count; 
+            int newIndex = 0;
+            while (handcards.ContainsKey(newIndex)) newIndex++;
+
             handcards.Add(newIndex, newCard);
+            Debug.Log($"[ADD] Card added to hand: {newCard.name}. (Hand size: {handcards.Count}/{maxHandCards})");
         }
     }
 
@@ -53,5 +63,35 @@ public class CardCounter : MonoBehaviour
             }
         }
         return maxHandCards;
+    }
+
+    private void PlayCardFromHandToBoard()
+    {
+        if (handcards.Count == 0) return;
+
+        int handKey = handcards.Keys.First(); 
+        Card cardToPlay = handcards[handKey];
+
+        int boardKey = 0;
+        while (boardcards.ContainsKey(boardKey)) boardKey++;
+
+        boardcards.Add(boardKey, cardToPlay); 
+        handcards.Remove(handKey);            
+
+        Debug.Log($"[PLAY] Card {cardToPlay.name} played to board (board key: {boardKey}).");
+    }
+
+    private void BurnCardFromBoard()
+    {
+        if (boardcards.Count == 0) return;
+
+        int boardKey = boardcards.Keys.First();
+        Card cardToBurn = boardcards[boardKey];
+
+        DeckManager.Instance.AddToDiscard(cardToBurn);
+
+        boardcards.Remove(boardKey);
+
+        Debug.Log($"[BURN] Card {cardToBurn.name} burned.");
     }
 }
